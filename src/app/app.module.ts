32f8f7c1137f6421309ba2ffb40edf804aa38a52
MaksymKutasievichDev <maksym.kutasievich.dev@gmail.com>
2022-12-28 17:59:23 +0100

    Initial settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarGuestComponent } from './components/navbar-guest/navbar-guest.component';
+import { NavbarAdminComponent } from './components/navbar-admin/navbar-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarGuestComponent,
+    NavbarAdminComponent
   ],
   imports: [
     BrowserModule,
